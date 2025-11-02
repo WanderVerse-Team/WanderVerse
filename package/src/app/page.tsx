595832c7017a422abc7ab7ceccdd04cfd33bf6cf
Moshtha <kdark2204@gmail.com>
2025-11-02 15:27:32 +0530

    Removed ceo part

--- package/src/app/page.tsx
+++ package/src/app/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Hero from '@/app/components/Home/Hero'
 import Aboutus from '@/app/components/Home/AboutUs'
-import Dedicated from '@/app/components/Home/Dedicated'
 import Digital from '@/app/components/Home/Digital'
 import Beliefs from '@/app/components/Home/Beliefs'
 import Work from '@/app/components/Home/Work'
@@ -23,9 +22,8 @@ export default function Home() {
   return (
     <main>
       <Hero />
-      <Aboutus />
-      <Dedicated />
-      <Digital />
+  <Aboutus />
+  <Digital />
       <Beliefs />
       <Work />
       <Team />
