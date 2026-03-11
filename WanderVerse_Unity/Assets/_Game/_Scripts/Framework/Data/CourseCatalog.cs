26cae3d996a071f5bba01cfdf0c7c79e1175e9be
RandivMendis <randivmendis14@gmail.com>
2026-03-11 12:00:06 +0530

    add level IDs to CourseCatalog

--- WanderVerse_Unity/Assets/_Game/_Scripts/Framework/Data/CourseCatalog.cs
+++ WanderVerse_Unity/Assets/_Game/_Scripts/Framework/Data/CourseCatalog.cs
@@ -4,23 +4,44 @@ namespace WanderVerse.Framework.Data
 {
     public static class CourseCatalog
     {
-        // The Game Levels for Grade 3 Mathematics
-        public const string L01_1 = "L01_Counting";
-        public const string L02 = "L02_Numbers1";
-        public const string L03 = "L03_Addition1";
-        public const string L04 = "L04_Length1";
-        public const string L05 = "L05_Subtraction1";
-        public const string L06 = "L06_Time";
-        public const string L07 = "L07_Multiplication1";
-        public const string L08 = "L08_Shapes";
-        public const string L09 = "L09_Division1";
-        public const string L10 = "L10_Fractions";
-        public const string L11 = "L11_Directions";
+        // Default unassigned state
+        public const string NONE = "None";
+
+        // Lesson 1: Counting - Hungry Golem
+        public const string L01_GOL_1 = "L01_GOL_1";
+        public const string L01_GOL_2 = "L01_GOL_2";
+        public const string L01_GOL_3 = "L01_GOL_3";
+
+        // Lesson 2: Numbers 1 - Talking Door
+        public const string L02_DOR_1 = "L02_DOR_1";
+        public const string L02_DOR_2 = "L02_DOR_2";
+
+        // Lesson 2: Numbers 1 - Treasure Packer
+        public const string L02_PAC_1 = "L02_PAC_1";
+        public const string L02_PAC_2 = "L02_PAC_2";
+
+        // Lesson 3: Addition 1 - Power Station
+        public const string L03_POW_1 = "L03_POW_1";
+        public const string L03_POW_2 = "L03_POW_2";
+        public const string L03_POW_3 = "L03_POW_3";
+
+        // Lesson 6: Time - Space Time Traveler
+        public const string L06_SPA_1 = "L06_SPA_1";
+
+        // Lesson 10: Fractions - Crumble's Bakery
+        public const string L10_BAK_1 = "L10_BAK_1";
+        public const string L10_BAK_2 = "L10_BAK_2";
+        public const string L10_BAK_3 = "L10_BAK_3";
 
         // This array is what the Unity Inspector will use to create the dropdown
         public static readonly string[] AllLevels = new string[]
         {
-            L01_1, L02, L03, L04, L05, L06, L07, L08, L09, L10, L11
+            NONE,
+            L01_GOL_1, L01_GOL_2, L01_GOL_3,
+            L02_DOR_1, L02_DOR_2, L02_PAC_1, L02_PAC_2,
+            L03_POW_1, L03_POW_2, L03_POW_3,
+            L06_SPA_1,
+            L10_BAK_1, L10_BAK_2, L10_BAK_3
         };
     }
 }
