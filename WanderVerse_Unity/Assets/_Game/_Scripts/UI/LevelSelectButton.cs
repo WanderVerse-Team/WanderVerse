59db82dd399c3d1bd83f67864747469f369206d2
RandivMendis <randivmendis14@gmail.com>
2026-03-28 14:01:36 +0530

    uncomment EnergyManager in LevelSelectButton

--- WanderVerse_Unity/Assets/_Game/_Scripts/UI/LevelSelectButton.cs
+++ WanderVerse_Unity/Assets/_Game/_Scripts/UI/LevelSelectButton.cs
@@ -26,8 +26,7 @@ public class LevelSelectButton : MonoBehaviour
             return;
         }
 
-        //bool canPlay = EnergyManager.Instance.TryConsumeEnergy();
-        bool canPlay = true;
+        bool canPlay = EnergyManager.Instance.TryConsumeEnergy();
 
         if (canPlay)
         {
